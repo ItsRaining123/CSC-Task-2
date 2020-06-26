add29de4c0e872da5f2ba773fbca1850fbec5a88
JOVIANLIM.17@ichat.sp.edu.sg <JOVIANLIM.17@ichat.sp.edu.sg>
2020-06-26 23:12:20 +0800

    Add project files.

---
+++ CSCTask2/Controllers/ProductsV2Controller.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CSCTask2.Models;
+
+namespace CSCTask2.Controllers
+{
+    public class ProductsV2Controller : ApiController
+    {
+        static readonly IProductRepository repository = new ProductRepository();
+        
+        [HttpGet]
+        [Route("api/v2/products")]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return repository.GetAll();
+        }
+
+        [HttpGet]
+        [Route("api/v2/products/{id:int:min(1)}")]
+        public Product GetProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return item;
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return repository.GetAll().Where(
+                p => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
+        }
+
+        [HttpPost]
+        [Route ("api/v2/products/create")]
+        public Product PostProduct(Product item)
+        {
+            item = repository.Add(item);
+            var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item);
+
+            //string uri = Url.Link("DefaultApi", new { id = item.Id });
+            //response.Headers.Location = new Uri(uri);
+            //return response;
+            return item;
+        }
+
+        public void PutProduct(int id, Product product)
+        {
+            product.Id = id;
+            if (!repository.Update(product))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        [HttpDelete]
+        [Route ("api/v2/products/delete/{id}")]
+        public void DeleteProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            repository.Remove(id);
+        }
+    }
+}
